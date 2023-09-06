3b8899ce4630823ec2d1d001dcd2ea65b85d4e72
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2023-09-06 22:42:27 +0200

    db: add join query :: need to fix

--- database/query.sql
+++ database/query.sql
@@ -2,7 +2,7 @@ SET search_path to castorodb;
 -- -----------------------------------------------------
 -- Query insert_user
 -- -----------------------------------------------------
-INSERT INTO user (nome, cognome, discord_name, lista_soprannomi)
+INSERT INTO "user" (nome, cognome, discord_name, lista_soprannomi)
 VALUES 
 ('lorenzo', 'mancini', 'manciolollo','mancio/sniccini'), 
 ('giovanni', 'rasera', 'cetriolinogio', 'cetriolino/mestolopiano');
@@ -14,3 +14,10 @@ INSERT INTO stories (title, story_date, story, tag)
 VALUES 
 ('fake', '2023-09-06', 'buonanotte ennio', 'mancio/cetriolino');
 
+-- join
+select "user".nome, stories.title 
+    from 
+        "user" join  user_has_stories  
+            on id_user = user_id_user 
+        join stories 
+            on id_story = stories_id_story;
