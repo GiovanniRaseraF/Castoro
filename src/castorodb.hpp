d4e89138356db6b3165f1779f40954450627314e
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2023-09-20 21:51:25 +0200

    db: first query

--- src/castorodb.hpp
+++ src/castorodb.hpp
@@ -26,18 +26,29 @@ class castorodb {
     /*
     */
     static std::vector<std::string> list_bros(){
+        std::string con_string = 
+        " user="        + user +
+        " password="    + password +
+        " host="        + hostaddr +
+        " port=5432 "   + 
+        " dbname="      + dbname;
+
         std::vector<std::string> ret{};
 
-        // pqxx::connection c{""};
-        // pqxx::work txn{c};
+        pqxx::connection c{con_string};
+        pqxx::work txn{c};
 
-        // for(auto row : txn.exec("SELECT nome FROM bro")){
-        //     std::string nome = (row["nome"].as<std::string>());
+        for(auto row : txn.exec("select * from castorodb.bro;")){
+            std::string nome = (row["nome"].as<std::string>());
 
-        //     ret.push_back(nome);
-        //     std::cout << nome << std::endl;
-        // }
+            ret.push_back(nome);
+        }
 
         return ret;
     }
 };
+
+std::string castorodb::dbname   {"postgres"};
+std::string castorodb::hostaddr {"localhost"};
+std::string castorodb::user     {"postgres"};
+std::string castorodb::password {"fdsa"};
